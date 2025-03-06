61a447d279dbd7f9c3593dea7483c666cc1b7659
kolesnikov-yuri <knyaz_vetrov@mail.ru>
2025-03-06 17:27:34 +0400

    part-1

---
+++ src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+// import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div>
+      <Navbar expand="lg" variant="dark" bg="dark">
+        <Container className="text-white">
+          <Navbar.Brand href="#home">Add to cart Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto w-100">
+              <NavLink to="/" className={"text-decoration-none text-white"}>
+                Product
+              </NavLink>
+              <NavLink className="w-100 text-decoration-none text-white">
+                <ShoppingCartIcon
+                  onClick={handleClick}
+                  style={{ float: "right" }}
+                />
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Your carts is empty</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default Header;
